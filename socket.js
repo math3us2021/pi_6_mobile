5512abe4d396d17b00c041cee476a4122cc57b85
matheus <matheusbf.mf@gmail.com>
2024-05-27 20:07:24 -0300

    socket

--- socket.js
+++ socket.js
@@ -1,3 +1,4 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://192.168.100.58:3000');
+export const socket = io('http://192.168.100.64:3000');
+// export const socket = io('http://192.168.100.58:3000');
